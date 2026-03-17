01aead6c931de18d53fda273e282b7b2057f100b
JonPlusPlus <jonathanfosterpayne@proton.me>
2026-03-17 21:34:47 +0000

    A ProgressBar that displays in the Kivy window.

---
+++ 11. Progress Bars/A. simple_progressbar.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+
+Window.clearcolor = (1, 1, 1, 1)
+
+
+class BasicProgressBar(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        cx = Window.width / 2
+        # ProgressBar(max=100, value=50): sets the maximum and current progress.
+        # size_hint=(None, None) and pos=(...): manually place the progress bar in the window.
+        self.pb = ProgressBar(max=100, value=50, size_hint=(None, None), width=300, height=30,pos=(cx - 150, Window.height - 100))
+        # add_widget(pb): adds the progress bar to the Kivy window.
+        self.add_widget(self.pb)
+
+
+class ProgressBarApp(App):
+    def build(self):
+        return BasicProgressBar()
+
+
+if __name__ == '__main__':
+    ProgressBarApp().run()
