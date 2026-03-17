bf80124729010c6b0c43d09c92272bbb524ab8f1
JonPlusPlus <jonathanfosterpayne@proton.me>
2026-03-17 21:35:56 +0000

    Added additional comments.

--- 11. Progress Bars/A. simple_progressbar.py
+++ 11. Progress Bars/A. simple_progressbar.py
@@ -13,6 +13,9 @@ class BasicProgressBar(Widget):
         # ProgressBar(max=100, value=50): sets the maximum and current progress.
         # size_hint=(None, None) and pos=(...): manually place the progress bar in the window.
         self.pb = ProgressBar(max=100, value=50, size_hint=(None, None), width=300, height=30,pos=(cx - 150, Window.height - 100))
+            #   max: Maximum allowed value (default 100).
+            #   value: Current value of the progress bar.
+            #   size_hint, width, height: manual sizing.
         # add_widget(pb): adds the progress bar to the Kivy window.
         self.add_widget(self.pb)
 
